3bd2e0ad74180d393689244b10a245ce9259ddbd
Matias Bacelar <matiasbacelar@gmail.com>
2023-06-17 17:34:14 -0300

    Fonts, icons & images

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,vue}'],
   theme: {
+    colors: {
+      black: '#000000',
+      white: '#FEFEFE',
+      'gray-light': '#C7C7C7',
+      'gray-medium': '#858585',
+      'gray-dark': '#2F2F2F',
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
