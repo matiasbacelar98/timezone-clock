ea4d50aaa7bc9a0b5ca2b2fcd0d6fee813ff7f7a
Matias Bacelar <matiasbacelar@gmail.com>
2023-06-21 22:21:49 -0300

    Reduce Quote component unnecessary requests

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -42,7 +42,7 @@ function toggleInfo() {
     class="font-poppins min-h-screen bg-[url(../assets/img/afternoon.jpg)] bg-cover bg-no-repeat bg-[50%_80%] py-[56px] flex flex-col px-6 justify-between md:px-[80px] lg:px-[120px] xl:px-[160px]"
     :class="{ relative: info.isOpen }"
   >
-    <Quote v-if="!info.isOpen" />
+    <Quote v-show="!info.isOpen" />
 
     <section class="md:flex md:justify-between mt-4">
       <Clock :province="info?.data?.countryAbbreviations?.capital" :country="info?.data?.countryAbbreviations?.fips" />
