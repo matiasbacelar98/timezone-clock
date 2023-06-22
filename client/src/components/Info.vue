0d0de1e77209e10977033abf82a05661d77d4ec0
Matias Bacelar <matiasbacelar@gmail.com>
2023-06-21 22:03:54 -0300

    Toggle ui improved

--- client/src/components/Info.vue
+++ client/src/components/Info.vue
@@ -13,9 +13,9 @@ const props = defineProps({
   <section class="absolute bottom-0 left-0 text-white bg-black w-full mt-14 py-14 px-6 bg-opacity-90 md:px-[80px] lg:px-[120px] xl:px-[160px]">
     <ul class="grid gap-y-6 sm:grid-cols-[1fr_auto_1fr] sm:gap-y-8 lg:gap-y-0">
       <InfoItem title="current timezone" :content="props.timezone" />
-      <InfoItem title="day of the year" content="121" />
+      <InfoItem title="day of the week" content="2" />
       <Line />
-      <InfoItem title="day of the week" content="2" :odd="true" />
+      <InfoItem title="day of the year" content="121" :odd="true" />
       <InfoItem title="week number" content="18" :odd="true" />
     </ul>
   </section>
